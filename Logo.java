33651f3e3e9c7d7e343af660eddfc71e2489caf1
younes921722 <younes.saouabeddine@gmail.com>
2023-03-11 22:29:13 +0100

    Adding new print line

--- Logo.java
+++ Logo.java
@@ -1,6 +1,7 @@
 class app{
 public static void main(String [] args){
 System.out.println("");
+System.out.println("Alhamdolillah ");
 
 }
 
--- app.java
+++ app.java
@@ -1,6 +1,7 @@
 class app{
 public static void main(String [] args){
-System.out.println("hello");
+System.out.println("hello world");
+System.out.println("Alhamdolillah ");
 
 }
 
