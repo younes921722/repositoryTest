02dd28e44237e236b67eff76e361d13bda899d1a
younes921722 <younes.saouabeddine2001@gmail.com>
2023-03-12 00:23:22 +0100

    new file

---
+++ new.java
@@ -0,0 +1,16 @@
+[B[Aclass app{
+public static void main(String [] args){
+System.out.println("");
+System.out.println("Alhamdolillah ");
+
+}
+
+
+}[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C()[D"hello");
+
+
+
+
+
+
+
